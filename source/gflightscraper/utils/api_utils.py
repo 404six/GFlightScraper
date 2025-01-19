9239eea072f2a637b4d4440c81837d4aad922dc6
404six <lucasmoraisc16@gmail.com>
2025-01-19 13:26:21 -0300

    FEAT(Utils): create wrapper for requests

---
+++ source/gflightscraper/utils/api_utils.py
@@ -0,0 +1,21 @@
+import requests
+
+from gflightscraper.config import DEFAULT_HEADERS
+
+def make_post_request(url, payload):
+    """
+    Makes a POST request to the specified URL with the given payload.
+
+    :param url: The URL to send the POST request to.
+    :param payload: The payload to send in the request body.
+    :param headers: Optional headers for the request.
+    :return: The response text from the API.
+    """
+    
+    try:
+        response = requests.post(url, data=payload, headers=DEFAULT_HEADERS)
+        response.raise_for_status()
+        return response.text
+    except requests.exceptions.RequestException as e:
+        print(f"Error making POST request: {e}")
+        return None
