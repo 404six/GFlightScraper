9f60b18e21a8dec3cb2ea35476fd71c4fe7ffbd3
404six <lucasmoraisc16@gmail.com>
2025-01-19 13:12:26 -0300

    FEAT: add config files with constants

---
+++ source/gflightscraper/config.py
@@ -0,0 +1,12 @@
+# Google Flights API Base URL
+GOOGLE_FLIGHTS_API_URL = "https://www.google.com/_/FlightsFrontendUi/data/"
+
+# Discord Webhook URL
+DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/your-webhook-url"
+
+DEFAULT_HEADERS = {
+    "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+}
+
+# Date and Time Formats
+DATE_FORMAT = "%Y-%m-%d"
