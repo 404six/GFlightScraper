01f5a71e8c4a996bfef5e713958d53a6e7a21cdf
404six <lucasmoraisc16@gmail.com>
2025-01-19 14:39:46 -0300

    Revert "FIX: fix constant strings usage"
    This reverts commit 192dbc3f2f448ab3ecd2925f48a7bb4225ea8d7a.

--- source/gflightscraper/utils/date_utils.py
+++ source/gflightscraper/utils/date_utils.py
@@ -1,7 +1,5 @@
 from datetime import datetime, timedelta
 
-from gflightscraper.config import DATE_FORMAT
-
 def calculate_date_ranges(start, end):
         """
         Calculates the date ranges based on the start and end dates provided. 
@@ -19,8 +17,8 @@ def calculate_date_ranges(start, end):
         max_date = today + timedelta(days=330)
 
         # Parse the input dates
-        start_date = datetime.strptime(start, DATE_FORMAT)
-        end_date = datetime.strptime(end, DATE_FORMAT)
+        start_date = datetime.strptime(start, "%Y-%m-%d")
+        end_date = datetime.strptime(end, "%Y-%m-%d")
 
         # You can't start with a date earlier than today
         if start_date < today:
@@ -32,11 +30,11 @@ def calculate_date_ranges(start, end):
         # Split the range based on the logic
         if total_days_in_range <= 199:
             date_ranges = [
-                (start_date.strftime(DATE_FORMAT), min(max_date, end_date).strftime(DATE_FORMAT))
+                (start_date.strftime("%Y-%m-%d"), min(max_date, end_date).strftime("%Y-%m-%d"))
             ]
         else:
             date_ranges = [
-                (start_date.strftime(DATE_FORMAT), (start_date + timedelta(days=199)).strftime(DATE_FORMAT)), 
-                ((start_date + timedelta(days=200)).strftime(DATE_FORMAT), min(max_date, end_date).strftime(DATE_FORMAT))
+                (start_date.strftime("%Y-%m-%d"), (start_date + timedelta(days=199)).strftime("%Y-%m-%d")), 
+                ((start_date + timedelta(days=200)).strftime("%Y-%m-%d"), min(max_date, end_date).strftime("%Y-%m-%d"))
             ]
         return date_ranges
